b9c8e69bf9f0458133275c247b11f1ed87d79a50
Navarette <navarette.arnold@iisgalvanimi.edu.it>
2022-11-07 09:41:43 +0000

    15.2

--- client-angular/src/app/foo/foo.component.ts
+++ client-angular/src/app/foo/foo.component.ts
@@ -1,30 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Foo } from './foo.module';
 
 @Component({
- selector: 'app-foo',
- templateUrl: './foo.component.html',
- styleUrls: ['./foo.component.css']
+  selector: 'app-foo',
+  templateUrl: './foo.component.html',
+  styleUrls: ['./foo.component.css']
 })
 export class FooComponent {
-   data!: Object;
-   loading!: boolean;
-   o!:Observable<Object>;
-   constructor(public http: HttpClient) {}
-   makeRequest(): void {
-     console.log("here");
-     this.loading = true;
-     this.o = this.http.get('https://jsonplaceholder.typicode.com/posts/1');
-     this.o.subscribe(this.getData);
-   }
-   getData = (d : Object) =>
-   {
-     this.data = new Object(d);
-     this.loading = false;
-   }
+  fooData!: Foo[];
+  data!: Object;
+  loading!: boolean;
+  o!: Observable<Object>;
+  oFoo!: Observable<Foo[]>;
+  
+  constructor(public http: HttpClient) { }
 
-   makeCompactRequest(): void {
+  makeRequest(): void {
+
+    this.loading = true;
+    this.o = this.http.get('https://jsonplaceholder.typicode.com/posts/1');
+    this.o.subscribe(this.getData);
+  }
+
+  getData = (d: Object) => {
+    this.data = d;
+    this.loading = false;
+  }
+
+  //Nota bene, questo è un metodo alternativo al metodo makeRequest
+  makeCompactRequest(): void {
+    this.loading = true;
+    this.http
+      .get('https://jsonplaceholder.typicode.com/posts/1')
+      .subscribe(data => {
+        this.data = data;
+        this.loading = false;
+      });
+  }
+
+  //L'operazione di post necessita un parametro in più.
+  //Viene creata una stringa (JSON.strigify) a partire da un oggetto Typescript
+  makeCompactPost(): void {
     this.loading = true;
     this.http
       .post('https://jsonplaceholder.typicode.com/posts',
@@ -37,6 +55,13 @@ export class FooComponent {
       .subscribe(data => {
         this.data = data;
         this.loading = false;
-       });
-      }
+      });
+  }
+
+  makeTypedRequest(): void {
+    //oFoo : Observable<Foo[]>; va dichiarato tra gli attributi della classe 
+    this.oFoo = this.http.get<Foo[]>('https://jsonplaceholder.typicode.com/posts');
+    this.oFoo.subscribe(data => { this.fooData = data; });
+  }
+
 }
