8baf5625b9003363edcf9035f582fd9d55f38d6b
Ghebr0us <ghebrous.davide@iisgalvanimi.edu.it>
2023-01-17 17:45:07 +0000

    .

--- AngularServer/src/app/genere-anime/genere-anime.component.ts
+++ AngularServer/src/app/genere-anime/genere-anime.component.ts
@@ -9,7 +9,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class GenereAnimeComponent {
   nome! : any;
-  url: string = "https://3000-navarette-otakupeak-wimjo9ufcxl.ws-eu82.gitpod.io/genereAnime";
+  url: string = "https://3000-ghebr0us-otakupeak-4a2vsftxhxu.ws-eu82.gitpod.io/genereAnime";
   
   constructor(public http : HttpClient){
     this.get(this.url);
--- AngularServer/src/app/titolo-manga/titolo-manga.component.ts
+++ AngularServer/src/app/titolo-manga/titolo-manga.component.ts
@@ -9,7 +9,7 @@ import { HttpClient } from '@angular/common/http';
 export class TitoloMangaComponent {
   titolom!: any;
   loading!: Boolean;
-  url: string = "https://3000-navarette-otakupeak-wimjo9ufcxl.ws-eu82.gitpod.io/titoloManga";
+  url: string = "https://3000-ghebr0us-otakupeak-4a2vsftxhxu.ws-eu82.gitpod.io/titoloManga";
 
   constructor(public http: HttpClient) {
     this.get(this.url);
