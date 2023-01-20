d0f8b5d9d13fbcb89f0f7ba8c5d78e6bfe28d4cd
Fabio Malizia <malizia.fabio@iisgalvanimi.edu.it>
2023-01-20 12:26:30 +0000

    punto 4

---
+++ correzione/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Post } from './models/post.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'correzione';
+
+  post_vect : Post[] = [];
+
+  invia(user : HTMLInputElement, message: HTMLInputElement) 
+  {
+    this.post_vect.push(new Post (user.value, message.value));
+    console.log(this.post_vect)
+  }
+}
---
+++ correzione/src/app/models/post.model.ts
@@ -0,0 +1,4 @@
+export class Post
+{
+    constructor(public user: string, public message:string){}
+}
